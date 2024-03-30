a188dc3e7e285a9c6c9898126a1aac0a19fd2d8f
Onur Baykal <theaob@gmail.com>
2024-03-30 14:40:38 +0300

    add random collectible generation

---
+++ Collectibles.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+const CHERRY_SCENE = preload("res://cherry.tscn")
+
+func _on_timer_timeout():
+	var cherry_instance = CHERRY_SCENE.instantiate()
+	var rng = RandomNumberGenerator.new()
+	var x_rand = rng.randi_range(0, 300)
+	
+	cherry_instance.position = Vector2(x_rand,390)
+	add_child(cherry_instance)
+	
