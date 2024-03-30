a8839e732de5ff423f1b38df97108cf233ea3b6f
Onur Baykal <theaob@gmail.com>
2024-03-30 14:31:55 +0300

    update cherry tween

--- cherry.gd
+++ cherry.gd
@@ -2,5 +2,11 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		queue_free()
-		Game.gold += 5
+		
+		Game.gold += 5		
+		var tween1 = get_tree().create_tween()
+		var tween2 = get_tree().create_tween()
+		tween1.tween_property(self, "position", position - Vector2(0,35),0.25)
+		tween2.tween_property(self, "modulate:a", 0, 0.35)
+		tween2.tween_callback(queue_free)
+		
